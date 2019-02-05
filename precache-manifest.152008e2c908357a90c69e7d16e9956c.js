447b0c65f8f8af09a6eefc4f25ec8b64168b1ff6
nyan lin tun <devkonyan@gmail.com>
2019-02-05 23:42:49 +0630

    Updates

---
+++ precache-manifest.152008e2c908357a90c69e7d16e9956c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d136ce4016908bf5b9d2",
+    "url": "/my-markdown/static/js/runtime~main.d136ce40.js"
+  },
+  {
+    "revision": "3bdcbafc13c2b105cf16",
+    "url": "/my-markdown/static/js/main.3bdcbafc.chunk.js"
+  },
+  {
+    "revision": "a5b4c1a773000022d5ed",
+    "url": "/my-markdown/static/js/1.a5b4c1a7.chunk.js"
+  },
+  {
+    "revision": "3bdcbafc13c2b105cf16",
+    "url": "/my-markdown/static/css/main.aac6897c.chunk.css"
+  },
+  {
+    "revision": "242e03463cd49273d84ff24649439054",
+    "url": "/my-markdown/index.html"
+  }
+];
